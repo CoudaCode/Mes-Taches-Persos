a0aed43beb247fb53cff249af670e3efb304ef3a
CoudaCode <couda.dm@gmail.com>
2023-10-22 17:18:30 +0000

    modif

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,27 @@
 <template>
   <div
     class="w-full rounded container bg-orange-950 h-4/5 items-center flex justify-center">
-    <div class="w-full max-w-lg text-center rounded">
-      <h1 class="text-3xl text-center">Dashboard</h1>
+    <div
+      class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+      <div class="md:flex">
+        <div class="md:shrink-0">
+          <img
+            class="h-48 w-full object-cover md:h-full md:w-48"
+            src=""
+            alt="Modern building architecture" />
+        </div>
+        <div class="p-8">
+          <div
+            class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+            Todo List
+          </div>
+          <router-link
+            to="/login"
+            class="py-2 px-4 bg-orange-500 text-white font-semibold rounded-lg shadow-md hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+            Connexion
+          </router-link>
+        </div>
+      </div>
     </div>
   </div>
 </template>
